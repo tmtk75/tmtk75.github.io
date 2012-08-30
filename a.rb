f2a0c92dac1231f02e94f322cb61fba6d9c58c20
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-08-30 09:45:32 +0900

    -

--- a.rb
+++ a.rb
@@ -1,8 +1,26 @@
-
+#!/usr/bin/env ruby
 
 path = ARGV[0]
 
 open(path, "r") {|f|
-  p f.read.gsub(/---.*?---/m, "").split("\n")
-  #.split(" +")
+  words = f.read
+           .gsub(/---.*?---/m, "")
+           .split(/ |\n/)
+           .map {|e| e.gsub /[^a-zA-Z0-9_\-]/, "-"}
+           .map {|e| e.gsub /-+/, "-"}
+           .map {|e| e.gsub /^-|-$/, ""}
+           .find_all {|e| e.length > 0}
+           .sort
+           .unshift([[]])
+           .reduce {|a, b|
+             if a.last.last == nil
+               a.last << b
+             elsif a.last.last == b
+               a.last << b
+             else
+               a << [b]
+             end
+             a
+           }
+  p words
 }
