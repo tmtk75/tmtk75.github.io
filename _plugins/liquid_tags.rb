8b79e981d199bc324954a8d324b57edf018bfb89
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-09-05 11:07:25 +0900

    -

--- _plugins/liquid_tags.rb
+++ _plugins/liquid_tags.rb
@@ -8,5 +8,20 @@ module Jekyll
       "#{@text} #{Time.now}"
     end
   end
+  module FooFilters
+    def foomomo(input)
+      "foo#{input}bar"
+    end
+  end
 end
 Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag)
+Liquid::Template.register_filter(Jekyll::FooFilters)
+
+module Jekyll
+  module AssetFilter
+    def asset_url(input)      
+      "http://www.example.com/#{input}?#{Time.now.to_i}"
+    end
+  end
+end
+Liquid::Template.register_filter(Jekyll::AssetFilter)
