b8f7584ce742e6e3979f0011006b7ea4905c187a
Tomotaka Sakuma <ktmtmks@gmail.com>
2012-12-07 18:19:06 +0900

    custom var

--- vendor/_ga.js
+++ vendor/_ga.js
@@ -1,6 +1,7 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-27755043-2']);
 _gaq.push(['_trackPageview']);
+_gaq.push(['_setCustomVar', 1, 'goal', 'jumly', 1]);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
