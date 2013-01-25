ad4822974e7f74630510d7f296b9316a1b9f58cb
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-01-25 23:09:54 +0900

    cohort sectioning

--- vendor/ga.js
+++ vendor/ga.js
@@ -4,6 +4,10 @@ _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
 _gaq.push(['_setAccount', 'UA-27755043-2']);
 _gaq.push(['_trackPageview']);
 _gaq.push(['_setCustomVar', 1, 'customvarteset', 'test' + (Math.floor(Math.random()*10)), 1]);
+var now = new Date;
+function zp(n) {return n/10 >= 1 ? n : "0" + n}
+var val = now.getFullYear() + zp(now.getMonth()) + "" + (Math.floor(getDate()/10) + 1)
+_gaq.push(['_setCustomVar', 2, 'cohort', val, 1]);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
