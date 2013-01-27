f8271be7d95a9a9d0ca592f9f93950403d174ec5
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-01-27 12:07:53 +0900

    fix cohort value like 2013003 -> 2013-01-3

--- vendor/ga.js
+++ vendor/ga.js
@@ -6,7 +6,7 @@ _gaq.push(['_trackPageview']);
 _gaq.push(['_setCustomVar', 1, 'customvarteset', 'test' + (Math.floor(Math.random()*10)), 1]);
 var now = new Date;
 function zp(n) {return n/10 >= 1 ? n : "0" + n}
-var val = now.getFullYear() + zp(now.getMonth()) + "" + (Math.floor(now.getDate()/10) + 1)
+var val = now.getFullYear() + "-" + zp(now.getMonth() + 1) + "-" + (Math.floor(now.getDate()/10) + 1)
 _gaq.push(['_setCustomVar', 2, 'cohort', val, 1]);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
