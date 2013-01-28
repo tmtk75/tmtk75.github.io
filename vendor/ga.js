2164d8d641a6d696604eeeeab728e2b4f903155c
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-01-28 11:31:02 +0900

    to delete index-1 of custom var

--- vendor/ga.js
+++ vendor/ga.js
@@ -3,10 +3,14 @@ var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
 _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
 _gaq.push(['_setAccount', 'UA-27755043-2']);
 _gaq.push(['_trackPageview']);
+
+_gaq.push(['_deleteCustomVar', 1]);
+
 var now = new Date;
 function zp(n) {return n/10 >= 1 ? n : "0" + n}
 var val = now.getFullYear() + "-" + zp(now.getMonth() + 1) + "-" + (Math.floor(now.getDate()/10) + 1)
 _gaq.push(['_setCustomVar', 2, 'cohort', val, 1]);
+
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
