df19f848c54896fcec0dc84f9154a67a8bd4ba26
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-04-07 20:42:29 +0900

    remove syntaxhighlighter

--- vendor/syntaxhighlighter.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  SyntaxHighlighter.config.clipboardSwf = '/vendor/javascripts/syntaxhighlighter/scripts/clipboard.swf';
-  SyntaxHighlighter.config.toolbar = false;
-  SyntaxHighlighter.all()
-});
