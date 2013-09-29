a15ed7a4ac669087a14f8fc947fb3f5344d40606
Tomotaka Sakuma <ktmtmks@gmail.com>
2013-09-29 23:17:11 +0900

    enable syntax highlight

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source :rubygems
 gem "middleman", "~> 3.1.4"
 gem "middleman-livereload", "~> 3.1.0"
 gem "middleman-blog", "~> 3.2.0"
+gem "middleman-syntax"
 gem "redcarpet"
 gem "slim"
 gem "stylus"
--- config.rb
+++ config.rb
@@ -16,6 +16,8 @@ Slim::Engine.set_default_options :shortcut => {
 set :markdown_engine, :redcarpet
 set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true
 
+activate :syntax
+
 # Time.zone = "UTC"
 
 activate :blog do |blog|
